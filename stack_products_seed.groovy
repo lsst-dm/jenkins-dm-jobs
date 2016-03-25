1c43cb6604eabdb84b33e4458d426be8abed520e
Joshua Hoblitt <josh@hoblitt.com>
2016-03-24 18:59:31 -0700

    create stack_products jobs from repos.yaml

--- stack_products_seed.groovy
+++ stack_products_seed.groovy
@@ -1,81 +1,114 @@
-job('ci_hsc-test') {
-  scm {
-    git {
-      remote {
-        github('jhoblitt/ci_hsc')
-        //refspec('+refs/pull/*:refs/remotes/origin/pr/*')
-      }
-      branch('*/master')
-      shallowClone(true)
-    }
-  }
+import org.yaml.snakeyaml.Yaml
 
-  parameters {
-    stringParam('BRANCH', null, "Whitespace delimited list of 'refs' to attempt to build.  Priority is highest -> lowest from left to right.  'master' is implicitly appended to the right side of the list, if not specified.")
-  }
+def repos = new URL("https://raw.githubusercontent.com/lsst/lsstsw/master/etc/repos.yaml")
 
-  properties {
-    rebuild {
-      autoRebuild()
-    }
-  }
-  concurrentBuild()
-  label('jenkins-master')
-  keepDependencies()
+def y  = new Yaml()
+def doc =  y.load(repos.newReader())
 
-  triggers {
-    cron('H 0,8,16 * * *')
-    githubPush()
-    pullRequest {
-      cron('H/5 * * * *')
-      useGitHubHooks()
-      permitAll()
-      // configure credential to use for GH API
-      configure { project ->
-        project / 'triggers' / 'org.jenkinsci.plugins.ghprb.GhprbTrigger' {
-          gitHubAuthId 'github-api-token-jhoblitt'
-        }
-      }
-    }
+def locations = [:]
+doc.each { k, v ->
+  def url
+  if (v instanceof LinkedHashMap) {
+    url = v['url']
+  } else {
+    url = v
   }
+  assert url
 
-  wrappers {
-    // 'The ghprbSourceBranch' variable is defined when the build was triggered
-    // by a GH PR.  If set, extract it for later use as the 'BRANCH' parameter
-    // to the 'stack-os-matrix'.  Otherwise, pass on the 'BRANCH' parameter
-    // this build was invoked with (it may be blank).
-    configure { project ->
-      project / 'buildWrappers' / 'EnvInjectBuildWrapper' / 'info' {
-        // yes, groovy heredocs are this lame...
-        groovyScriptContent """
-if (binding.variables.containsKey('ghprbSourceBranch')) {
-  return [BUILD_BRANCH: ghprbSourceBranch]
-} else {
-  return [BUILD_BRANCH: BRANCH]
+  // convert GH url into project slug
+  def m = url =~ /^http[s]:\/\/github.com\/([^.\/]*\/[^.\/]*)(\.git)?$/
+  assert m
+  def slug = m[0][1]
+  assert slug
+
+  locations[k] = slug
 }
-"""
+
+locations.each { name,slug ->
+  println "${name} : ${slug}"
+  stack_job(name, slug)
+}
+
+def stack_job(String name, String slug) {
+  job(name) {
+    scm {
+      git {
+        remote {
+          github(slug)
+          //refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+        }
+        branch('*/master')
+        shallowClone(true)
       }
     }
-  }
 
-  steps {
-    downstreamParameterized {
-      trigger('stack-os-matrix') {
-        block {
-          buildStepFailure('FAILURE')
-          failure('FAILURE')
+    parameters {
+      stringParam('BRANCH', null, "Whitespace delimited list of 'refs' to attempt to build.  Priority is highest -> lowest from left to right.  'master' is implicitly appended to the right side of the list, if not specified.")
+    }
+
+    properties {
+      rebuild {
+        autoRebuild()
+      }
+    }
+    concurrentBuild()
+    label('jenkins-master')
+    keepDependencies()
+
+    triggers {
+      cron('H 0,8,16 * * *')
+      githubPush()
+      pullRequest {
+        cron('H/5 * * * *')
+        useGitHubHooks()
+        permitAll()
+        // configure credential to use for GH API
+        configure { project ->
+          project / 'triggers' / 'org.jenkinsci.plugins.ghprb.GhprbTrigger' {
+            gitHubAuthId 'github-api-token-jhoblitt'
+          }
         }
-        parameters {
-          //currentBuild()
-          predefinedProps([
-            PRODUCT: 'ci_hsc',
-            BRANCH: '$BUILD_BRANCH'
-          ])
-          booleanParam('SKIP_DEMO', true)
+      }
+    }
+
+    wrappers {
+      // 'The ghprbSourceBranch' variable is defined when the build was triggered
+      // by a GH PR.  If set, extract it for later use as the 'BRANCH' parameter
+      // to the 'stack-os-matrix'.  Otherwise, pass on the 'BRANCH' parameter
+      // this build was invoked with (it may be blank).
+      configure { project ->
+        project / 'buildWrappers' / 'EnvInjectBuildWrapper' / 'info' {
+          // yes, groovy heredocs are this lame...
+          groovyScriptContent """
+  if (binding.variables.containsKey('ghprbSourceBranch')) {
+    return [BUILD_BRANCH: ghprbSourceBranch]
+  } else {
+    return [BUILD_BRANCH: BRANCH]
+  }
+  """
         }
-        publishers {
-          // must be defined even to use the global defaults
-          hipChat {}
+      }
+    }
+
+    steps {
+      downstreamParameterized {
+        trigger('stack-os-matrix') {
+          block {
+            buildStepFailure('FAILURE')
+            failure('FAILURE')
+          }
+          parameters {
+            //currentBuild()
+            predefinedProps([
+              PRODUCT: 'ci_hsc',
+              BRANCH: '$BUILD_BRANCH'
+            ])
+            booleanParam('SKIP_DEMO', true)
+          }
+          publishers {
+            // must be defined even to use the global defaults
+            hipChat {}
+          }
         }
       }
     }
