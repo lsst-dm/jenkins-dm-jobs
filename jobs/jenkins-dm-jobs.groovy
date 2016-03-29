552e215d4962263c6136d529345e047e78519a5e
Joshua Hoblitt <josh@hoblitt.com>
2016-03-28 18:22:56 -0700

    add jenkins-dm-jobs.groovy
    Tests the job repo

---
+++ jobs/jenkins-dm-jobs.groovy
@@ -0,0 +1,51 @@
+def name = 'jenkins-dm-jobs'
+def org = 'jhoblitt'
+def slug = "${org}/${name}"
+
+folder('ci-ci') {
+  description('CI for the CI system(s)')
+}
+
+job("ci-ci/${name}") {
+  scm {
+    git {
+      remote {
+        github(slug)
+        //refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+      }
+      branch('*/master')
+      shallowClone(true)
+    }
+  }
+
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+  concurrentBuild()
+
+  triggers {
+    githubPush()
+    pullRequest {
+      cron('H/5 * * * *')
+      useGitHubHooks()
+      permitAll()
+      // configure credential to use for GH API
+      configure { project ->
+        project / 'triggers' / 'org.jenkinsci.plugins.ghprb.GhprbTrigger' {
+          gitHubAuthId 'github-api-token-jhoblitt'
+        }
+      }
+    }
+  }
+
+  steps {
+    shell('./gradlew test')
+  }
+
+  publishers {
+    // must be defined even to use the global defaults
+    hipChat {}
+  }
+}
