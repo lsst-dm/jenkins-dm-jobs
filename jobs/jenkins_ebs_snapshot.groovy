af6f3cd99949ef95d528844abeea3f84ce79d7ca
Joshua Hoblitt <josh@hoblitt.com>
2016-10-05 09:19:12 -0700

    add jenkins-ebs-snapshot job
    Converted from https://github.com/lsst-sqre/sandbox-jenkins-demo/blob/32e3596f2f6ff388f42e5c66d373f625cbde8a3f/jenkins_demo/templates/jobs/jenkins-ebs-snapshot/config.xml

---
+++ jobs/jenkins_ebs_snapshot.groovy
@@ -0,0 +1,52 @@
+def name = 'ec2-snapshot'
+def org = 'lsst-sqre'
+def slug = "${org}/${name}"
+
+job('ci-ci/jenkins-ebs-snapshot') {
+  scm {
+    git {
+      remote {
+        github(slug)
+        //refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+      }
+      branch('*/master')
+      extensions {
+        cloneOptions {
+          shallow(true)
+        }
+      }
+    }
+  }
+
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+
+  // must run on jenkins master node
+  label('jenkins-master')
+
+  triggers {
+    cron('0 0 * * *')
+  }
+
+  wrappers {
+    credentialsBinding {
+      usernamePassword(
+        'AWS_ACCESS_KEY_ID',
+        'AWS_SECRET_ACCESS_KEY',
+        'jenkins-aws'
+      )
+    }
+  }
+
+  steps {
+    shell('sh ec2-snapshot.sh')
+  }
+
+  publishers {
+    // must be defined even to use the global defaults
+    hipChat {}
+  }
+}
