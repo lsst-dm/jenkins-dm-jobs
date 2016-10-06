23d7301db43a0509d7e7e061b6f10b9afa244f1c
Joshua Hoblitt <josh@hoblitt.com>
2016-10-06 16:35:18 -0700

    add a ci_hsc "stack wrapper"

--- jobs/stack_wrappers.groovy
+++ jobs/stack_wrappers.groovy
@@ -30,6 +30,10 @@ import util.StackOsMatrix
     product: 'sims_utils',
     skip_demo: true,
   ],
+  [
+    product: 'ci_hsc',
+    skip_demo: true,
+  ],
 ].each { j ->
   def stack = new StackOsMatrix(j)
 
