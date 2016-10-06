7e6f9eafafa3cb3819eb07abe4a216c5c1c622dd
Joshua Hoblitt <josh@hoblitt.com>
2016-10-06 08:34:34 -0700

    add test-pipeline job

---
+++ jobs/test_pipeline.groovy
@@ -0,0 +1,24 @@
+pipelineJob('ci-ci/test-pipeline') {
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+
+  def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+  def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url(repo)
+          }
+          branch(ref)
+        }
+      }
+      scriptPath('pipelines/test_pipe.groovy')
+    }
+  }
+}
---
+++ pipelines/test_pipe.groovy
@@ -0,0 +1,42 @@
+def notify = null
+
+node('jenkins-master') {
+  dir('jenkins-dm-jobs') {
+    // XXX the git step seemed to blowup on a branch of '*/<foo>'
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs()
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+  }
+}
+
+try {
+  notify.started()
+
+  stage 'yo'
+
+  echo 'foo'
+
+} catch (e) {
+  // If there was an exception thrown, the build failed
+  currentBuild.result = "FAILED"
+  throw e
+} finally {
+  echo "result: ${currentBuild.result}"
+  switch(currentBuild.result) {
+    case null:
+    case 'SUCCESS':
+      notify.success()
+      break
+    case 'ABORTED':
+      notify.aborted()
+      break
+    case 'FAILURE':
+      notify.failure()
+      break
+    default:
+      notify.failure()
+  }
+}
