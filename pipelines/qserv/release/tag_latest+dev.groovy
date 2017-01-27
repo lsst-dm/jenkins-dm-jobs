3d5fb520453d7510058bc6f03e9c997c4acc2111
Joshua Hoblitt <josh@hoblitt.com>
2017-01-27 15:58:12 -0700

    add qserv/release/tag-latest+dev job
    rebuild/tag `qserv_distrib` as both `qserv_latest` and `qserv-dev`.  The
    same `bNNNN` is used for both jobs for efficiency and to ensure that the
    tags are pointing to identical `eupspkg`s.

---
+++ pipelines/qserv/release/tag_latest+dev.groovy
@@ -0,0 +1,116 @@
+def notify = null
+node {
+  dir('jenkins-dm-jobs') {
+    // XXX the git step seemed to blowup on a branch of '*/<foo>'
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs()
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+  }
+}
+
+try {
+  notify.started()
+
+  def branch  = ''
+  def product = 'qserv_distrib'
+
+  def bx = null
+  def rebuildId = null
+  def buildJob = 'run-rebuild'
+  def publishJob = 'run-publish'
+
+  stage('build') {
+    def result = build job: buildJob,
+        parameters: [
+          string(name: 'BRANCH', value: branch),
+          string(name: 'PRODUCT', value: product),
+          booleanParam(name: 'SKIP_DEMO', value: true),
+          booleanParam(name: 'SKIP_DOCS', value: true)
+        ],
+        wait: true
+    rebuildId = result.id
+  }
+
+  stage('parse bNNNN') {
+    node {
+      manifest_artifact = 'lsstsw/build/manifest.txt'
+
+      step ([$class: 'CopyArtifact',
+            projectName: buildJob,
+            filter: manifest_artifact,
+            selector: [$class: 'SpecificBuildSelector', buildNumber: rebuildId]
+            ]);
+
+      def manifest = readFile manifest_artifact
+      bx = bxxxx(manifest)
+
+      echo "parsed bxxxx: ${bx}"
+    }
+  }
+
+  tagProduct(bx, 'qserv-dev', 'qserv_distrib')
+  tagProduct(bx, 'qserv_latest', 'qserv_distrib')
+
+  stage('archive') {
+    node {
+      results = [
+        bnnnn: bx
+      ]
+      dumpJson('results.json', results)
+
+      archiveArtifacts([
+        artifacts: 'results.json',
+        fingerprint: true
+      ])
+    }
+  }
+} catch (e) {
+  // If there was an exception thrown, the build failed
+  currentBuild.result = "FAILED"
+  throw e
+} finally {
+  echo "result: ${currentBuild.result}"
+  switch(currentBuild.result) {
+    case null:
+    case 'SUCCESS':
+      notify.success()
+      break
+    case 'ABORTED':
+      notify.aborted()
+      break
+    case 'FAILURE':
+      notify.failure()
+      break
+    default:
+      notify.failure()
+  }
+}
+
+def tagProduct(String buildId, String eupsTag, String product) {
+  stage("eups publish [${eupsTag}]") {
+    build job: publishJob,
+      parameters: [
+        string(name: 'EUPSPKG_SOURCE', value: 'git'),
+        string(name: 'BUILD_ID', value: buildId),
+        string(name: 'TAG', value: eupsTag),
+        string(name: 'PRODUCT', value: product)
+      ]
+  }
+}
+
+@NonCPS
+def bxxxx(manifest) {
+  def m = manifest =~ /(?m)^BUILD=(b.*)/
+  m ? m[0][1] : null
+}
+
+@NonCPS
+def dumpJson(String filename, Map data) {
+  def json = new groovy.json.JsonBuilder(data)
+  def pretty = groovy.json.JsonOutput.prettyPrint(json.toString())
+  echo pretty
+  writeFile file: filename, text: pretty
+}
