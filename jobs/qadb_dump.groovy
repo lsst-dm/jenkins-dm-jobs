fe8a4a3990b0209c3e2264d096b964c5ab454488
Joshua Hoblitt <josh@hoblitt.com>
2017-02-08 11:58:38 -0700

    add build-mysqldump-to-s3 job

--- jobs/qadb_dump.groovy
+++ jobs/qadb_dump.groovy
@@ -35,3 +35,34 @@ pipelineJob('backup/qadb-dump') {
     }
   }
 }
+
+pipelineJob('backup/build-mysqldump-to-s3') {
+  description('Constructs lsstsqre/mysqldump-to-s3 container.')
+
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+
+  label('jenkins-master')
+  concurrentBuild(false)
+  keepDependencies(true)
+
+  def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+  def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url(repo)
+          }
+          branch(ref)
+        }
+      }
+      scriptPath('pipelines/backup/build_mysqldump_to_s3.groovy')
+    }
+  }
+}
