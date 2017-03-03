7fd6ebd31b6c5331ae6b1de45979ebe3d30f5dfd
Joshua Hoblitt <josh@hoblitt.com>
2017-03-03 15:57:55 -0700

    remove lsst_sims from default product list

--- jobs/stack_osx.groovy
+++ jobs/stack_osx.groovy
@@ -3,7 +3,7 @@ import util.Common
 def j = matrixJob('stack-osx') {
   parameters {
     stringParam('BRANCH', null, 'Whitespace delimited list of "refs" to attempt to build.  Priority is highest -> lowest from left to right.  "master" is implicitly appended to the right side of the list, if not specified.')
-    stringParam('PRODUCT', 'lsst_sims lsst_distrib', 'Whitespace delimited list of EUPS products to build.')
+    stringParam('PRODUCT', 'lsst_distrib', 'Whitespace delimited list of EUPS products to build.')
     booleanParam('SKIP_DEMO', false, 'Do not run the demo after all packages have completed building.')
     booleanParam('NO_FETCH', false, 'Do not pull from git remote if branch is already the current ref. (This should generally be false outside of testing the CI system)')
   }
