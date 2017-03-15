cc1439e80c5219c37262eaedd8af95463a6ea163
Joshua Hoblitt <josh@hoblitt.com>
2017-03-15 14:30:58 -0700

    add release/tarball job
    Builds and pushes to s3 (publishes) EUPS Linux64 and DarwinX86 flavor
    "tarball" packages.

---
+++ jobs/tarball.groovy
@@ -0,0 +1,39 @@
+import util.Common
+Common.makeFolders(this)
+
+pipelineJob('release/tarball') {
+  description('build and publish EUPS distrib "tarball" packages')
+
+  parameters {
+    stringParam('PRODUCT', 'lsst_distrib', 'Whitespace delimited list of EUPS products to build.')
+    stringParam('EUPS_TAG', null, 'published EUPS tag')
+  }
+
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+
+  // don't tie up a beefy build slave
+  label('jenkins-master')
+  concurrentBuild(false)
+  keepDependencies(true)
+
+  def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+  def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url(repo)
+          }
+          branch(ref)
+        }
+      }
+      scriptPath('pipelines/release/tarball.groovy')
+    }
+  }
+}
