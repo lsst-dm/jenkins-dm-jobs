3d76bcf06344f223acf84758ab6fa3dfd50497d5
Joshua Hoblitt <josh@hoblitt.com>
2017-03-20 17:17:10 -0700

    fix typo

--- jobs/run_rebuild.groovy
+++ jobs/run_rebuild.groovy
@@ -2,7 +2,7 @@ import util.Common
 Common.makeFolders(this)
 
 pipelineJob('release/run-rebuild') {
-  description('run rebuild in the canoncial LSST DM build environment.  Only us this job when preparing to publish EUPS distrib packages.)
+  description('run rebuild in the canoncial LSST DM build environment.  Only us this job when preparing to publish EUPS distrib packages.')
 
   parameters {
     stringParam('BRANCH', null, 'Whitespace delimited list of "refs" to attempt to build.  Priority is highest -> lowest from left to right.  "master" is implicitly appended to the right side of the list, if not specified.')
