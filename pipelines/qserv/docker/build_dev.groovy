a571fd042e0d54ebf6754a9fd1189c89193abaf4
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2017-03-31 16:34:35 +0200

    Separate tasks for release and development images creation

---
+++ pipelines/qserv/docker/build_dev.groovy
@@ -0,0 +1,61 @@
+def notify = null
+node {
+  dir('jenkins-dm-jobs') {
+    // XXX the git step seemed to blowup on a branch of '*/<foo>'
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs()
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+  }
+}
+
+try {
+  notify.started()
+
+  node('docker') {
+    dir('qserv') {
+      git([
+        url: 'https://github.com/lsst/qserv.git',
+        branch: 'master'
+      ])
+    }
+
+    build('dev_images.sh')
+  }
+} catch (e) {
+  // If there was an exception thrown, the build failed
+  currentBuild.result = "FAILED"
+  throw e
+} finally {
+  echo "result: ${currentBuild.result}"
+  switch(currentBuild.result) {
+    case null:
+    case 'SUCCESS':
+      notify.success()
+      break
+    case 'ABORTED':
+      notify.aborted()
+      break
+    case 'FAILURE':
+      notify.failure()
+      break
+    default:
+      notify.failure()
+  }
+}
+
+def build(String script) {
+  stage(script) {
+    docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-sqreadmin') {
+      dir('qserv/admin/tools/docker/lsst-dm-ci') {
+        withEnv(['DOCKER_REPO=qserv/qserv']) {
+          wrap([$class: 'AnsiColorBuildWrapper']) {
+            sh "./$script"
+          }
+        }
+      }
+    }
+  }
+}
