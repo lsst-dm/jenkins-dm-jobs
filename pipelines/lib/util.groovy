9bcf92664f5645b25faef8b319b9bc8356491df2
Joshua Hoblitt <josh@hoblitt.com>
2017-07-03 11:04:59 -0700

    factor wrapContainer() out into a "util" lib
    This is to allow reuse between multiple pipeline scripts.

---
+++ pipelines/lib/util.groovy
@@ -0,0 +1,49 @@
+/**
+ * Create a thin "wrapper" container around {@code imageName} to map uid/gid of
+ * the user invoking docker into the container.
+ *
+ * @param imageName docker image slug
+ * @param tag name of tag to apply to generated image
+ */
+def void wrapContainer(String imageName, String tag) {
+  def buildDir = 'docker'
+  def config = dedent("""
+    FROM    ${imageName}
+
+    ARG     USER
+    ARG     UID
+    ARG     GROUP
+    ARG     GID
+    ARG     HOME
+
+    USER    root
+    RUN     groupadd -g \$GID \$GROUP
+    RUN     useradd -d \$HOME -g \$GROUP -u \$UID \$USER
+
+    USER    \$USER
+    WORKDIR \$HOME
+  """)
+
+  // docker insists on recusrively checking file access under its execution
+  // path -- so run it from a dedicated dir
+  dir(buildDir) {
+    writeFile(file: 'Dockerfile', text: config)
+
+    shColor """
+      set -e
+      set -x
+
+      docker build -t "${tag}" \
+          --build-arg USER="\$(id -un)" \
+          --build-arg UID="\$(id -u)" \
+          --build-arg GROUP="\$(id -gn)" \
+          --build-arg GID="\$(id -g)" \
+          --build-arg HOME="\$HOME" \
+          .
+    """
+
+    deleteDir()
+  }
+}
+
+return this;
