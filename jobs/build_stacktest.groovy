8fb0a69c310d502d9506ba9a22133f1b4aad0ee1
Joshua Hoblitt <josh@hoblitt.com>
2017-07-21 16:28:18 -0700

    add build-stacktest job

---
+++ jobs/build_stacktest.groovy
@@ -0,0 +1,39 @@
+import util.Common
+Common.makeFolders(this)
+
+def folder = 'sqre/infrastructure'
+
+pipelineJob("${folder}/build-stacktest") {
+  description('Constructs docker images with EUPS tarballs.')
+
+  parameters {
+    stringParam('TAG', null, 'EUPS distrib tag name. Eg. w_2016_08')
+  }
+
+  properties {
+    rebuild {
+      autoRebuild()
+    }
+  }
+
+  label('jenkins-master')
+  keepDependencies(true)
+  concurrentBuild(false)
+
+  def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+  def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote {
+            url(repo)
+          }
+          branch(ref)
+        }
+      }
+      scriptPath("pipelines/${folder}/build_stacktest.groovy")
+    }
+  }
+}
---
+++ pipelines/sqre/infrastructure/build_stacktest.groovy
@@ -0,0 +1,72 @@
+def notify = null
+node {
+  dir('jenkins-dm-jobs') {
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs()
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+    util = load 'pipelines/lib/util.groovy'
+  }
+}
+
+try {
+  notify.started()
+
+  def tag = params.TAG
+  def baseImage = 'lsstsqre/centos:7-stackbase'
+  def hubRepo = 'lsstsqre/stacktest'
+  def slug = "${hubRepo}:7-stack-lsst_distrib-${params.TAG}"
+
+  if (!params.TAG) {
+    error 'TAG parameter is required'
+  }
+
+  node('docker') {
+    stage('checkout') {
+      git([
+        url: 'https://github.com/lsst-sqre/docker-tarballs',
+        branch: 'master'
+      ])
+    }
+
+    stage('pull') {
+      def image = docker.image(baseImage)
+      image.pull()
+    }
+
+    stage('build') {
+      util.shColor "docker build --build-arg TAG=\"${tag}\" -t \"${slug}\" ."
+    }
+
+    stage('push') {
+      docker.withRegistry(
+        'https://index.docker.io/v1/',
+        'dockerhub-sqreadmin'
+      ) {
+        util.shColor "docker push \"${slug}\""
+      }
+    }
+  }
+} catch (e) {
+  // If there was an exception thrown, the build failed
+  currentBuild.result = "FAILED"
+  throw e
+} finally {
+  echo "result: ${currentBuild.result}"
+  switch(currentBuild.result) {
+    case null:
+    case 'SUCCESS':
+      notify.success()
+      break
+    case 'ABORTED':
+      notify.aborted()
+      break
+    case 'FAILURE':
+      notify.failure()
+      break
+    default:
+      notify.failure()
+  }
+}
