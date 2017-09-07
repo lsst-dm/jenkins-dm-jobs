d9ba21367e1f812466cf9cf70e8fda85f32d4bea
Joshua Hoblitt <josh@hoblitt.com>
2017-09-06 18:24:49 -0700

    groovy doesn't not allow unless with an else block

--- pipelines/release/run_publish.groovy
+++ pipelines/release/run_publish.groovy
@@ -78,7 +78,7 @@ try {
       } // stage('publish')
 
       stage('push packages') {
-        unless (params.NO_PUSH) {
+        if (! params.NO_PUSH) {
           withCredentials([[
             $class: 'UsernamePasswordMultiBinding',
             credentialsId: 'aws-eups-push',
