f00b80b8342f178c48912ea21d959bed4870cf2b
Joshua Hoblitt <josh@hoblitt.com>
2017-09-21 10:20:59 -0700

    rm unnecessary LSSTSW setup
    This was a left over artifact from lsst-dev.

--- pipelines/release/run_rebuild.groovy
+++ pipelines/release/run_rebuild.groovy
@@ -63,10 +63,6 @@ try {
                 util.shColor '''
                   #!/bin/bash -e
 
-                  # ensure that we are using the lsstsw clone relative to the workspace
-                  # and that another value for LSSTSW isn't leaking in from the env
-                  export LSSTSW="${WORKSPACE}/lsstsw"
-
                   if [[ -e "${WORKSPACE}/REPOS" ]]; then
                     export REPOSFILE="${WORKSPACE}/REPOS"
                   fi
