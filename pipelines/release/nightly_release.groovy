0c5358e28c8942c5286e46554e87eac185dde09c
Joshua Hoblitt <josh@hoblitt.com>
2017-09-26 07:57:26 -0700

    always archive 'results.json', even upon failure

--- pipelines/release/nightly_release.groovy
+++ pipelines/release/nightly_release.groovy
@@ -17,190 +17,201 @@ node('jenkins-master') {
 try {
   notify.started()
 
-  def gitTag = null
-  def eupsTag = null
-  def product = 'lsst_distrib qserv_distrib'
-  def retries = 3
-  def bx = null
-  def rebuildId = null
-  def buildJob = 'release/run-rebuild'
-  def publishJob = 'release/run-publish'
+  try {
+    def gitTag = null
+    def eupsTag = null
+    def product = 'lsst_distrib qserv_distrib'
+    def retries = 3
+    def bx = null
+    def rebuildId = null
+    def buildJob = 'release/run-rebuild'
+    def publishJob = 'release/run-publish'
 
-  def year = null
-  def month = null
-  def day = null
+    def year = null
+    def month = null
+    def day = null
 
-  stage('format nightly tag') {
-    if (!params.YEAR) {
-      error 'YEAR parameter is required'
+    stage('format nightly tag') {
+      if (!params.YEAR) {
+        error 'YEAR parameter is required'
+      }
+      if (!params.MONTH) {
+        error 'MONTH parameter is required'
+      }
+      if (!params.DAY) {
+        error 'DAY parameter is required'
+      }
+
+      year = params.YEAR.padLeft(4, "0")
+      month = params.MONTH.padLeft(2, "0")
+      day = params.DAY.padLeft(2, "0")
+
+      gitTag = "d.${year}.${month}.${day}"
+      echo "generated [git] tag: ${gitTag}"
+
+      // eups doesn't like dots in tags, convert to underscores
+      eupsTag = gitTag.tr('.', '_')
+      echo "generated [eups] tag: ${eupsTag}"
     }
-    if (!params.MONTH) {
-      error 'MONTH parameter is required'
-    }
-    if (!params.DAY) {
-      error 'DAY parameter is required'
-    }
 
-    year = params.YEAR.padLeft(4, "0")
-    month = params.MONTH.padLeft(2, "0")
-    day = params.DAY.padLeft(2, "0")
-
-    gitTag = "d.${year}.${month}.${day}"
-    echo "generated [git] tag: ${gitTag}"
-
-    // eups doesn't like dots in tags, convert to underscores
-    eupsTag = gitTag.tr('.', '_')
-    echo "generated [eups] tag: ${eupsTag}"
-  }
-
-  stage('build') {
-    retry(retries) {
-      timeout(time: 6, unit: 'HOURS') {
-        def result = build job: buildJob,
-          parameters: [
-            string(name: 'PRODUCT', value: product),
-            booleanParam(name: 'SKIP_DEMO', value: false),
-            booleanParam(name: 'SKIP_DOCS', value: true),
-          ],
-          wait: true
-        rebuildId = result.id
+    stage('build') {
+      retry(retries) {
+        timeout(time: 6, unit: 'HOURS') {
+          def result = build job: buildJob,
+            parameters: [
+              string(name: 'PRODUCT', value: product),
+              booleanParam(name: 'SKIP_DEMO', value: false),
+              booleanParam(name: 'SKIP_DOCS', value: true),
+            ],
+            wait: true
+          rebuildId = result.id
+        }
       }
     }
-  }
 
-  stage('parse bNNNN') {
-    node {
-      manifest_artifact = 'lsstsw/build/manifest.txt'
+    stage('parse bNNNN') {
+      node {
+        manifest_artifact = 'lsstsw/build/manifest.txt'
 
-      step([$class: 'CopyArtifact',
-            projectName: buildJob,
-            filter: manifest_artifact,
-            selector: [$class: 'SpecificBuildSelector', buildNumber: rebuildId]
+        step([$class: 'CopyArtifact',
+              projectName: buildJob,
+              filter: manifest_artifact,
+              selector: [
+                $class: 'SpecificBuildSelector',
+                buildNumber: rebuildId
+              ],
             ])
 
-      def manifest = readFile manifest_artifact
-      bx = util.bxxxx(manifest)
+        def manifest = readFile manifest_artifact
+        bx = util.bxxxx(manifest)
 
-      echo "parsed bxxxx: ${bx}"
+        echo "parsed bxxxx: ${bx}"
+      }
     }
-  }
 
-  stage('eups publish') {
-    def pub = [:]
+    stage('eups publish') {
+      def pub = [:]
 
-    pub[eupsTag] = {
+      pub[eupsTag] = {
+        retry(retries) {
+          timeout(time: 1, unit: 'HOURS') {
+            util.tagProduct(bx, eupsTag, product, publishJob)
+          }
+        }
+      }
+      pub['d_latest'] = {
+        retry(retries) {
+          timeout(time: 1, unit: 'HOURS') {
+            util.tagProduct(bx, 'd_latest', product, publishJob)
+          }
+        }
+      }
+
+      parallel pub
+    }
+
+    stage('wait for s3 sync') {
+      sleep time: 15, unit: 'MINUTES'
+    }
+
+    // NOOP / DRY_RUN
+    stage('git tag') {
       retry(retries) {
         timeout(time: 1, unit: 'HOURS') {
-          util.tagProduct(bx, eupsTag, product, publishJob)
+          // needs eups distrib tag to be sync'd from s3 -> k8s volume
+          build job: 'release/tag-git-repos',
+            parameters: [
+              string(name: 'BUILD_ID', value: bx),
+              string(name: 'GIT_TAG', value: gitTag),
+              booleanParam(name: 'DRY_RUN', value: true)
+            ]
         }
       }
     }
-    pub['d_latest'] = {
-      retry(retries) {
-        timeout(time: 1, unit: 'HOURS') {
-          util.tagProduct(bx, 'd_latest', product, publishJob)
-        }
-      }
-    }
 
-    parallel pub
-  }
+    stage("build eups tarballs") {
+      def operatingsystem = [
+        'centos-7',
+        'centos-6',
+        'osx-10.11',
+      ]
 
-  stage('wait for s3 sync') {
-    sleep time: 15, unit: 'MINUTES'
-  }
+      def pyenv = [
+        new MinicondaEnv('2', '4.2.12', '7c8e67'), // keep until v14_0
+        new MinicondaEnv('3', '4.2.12', '7c8e67'), // keep until v14_0
+        new MinicondaEnv('2', '4.3.21', '10a4fa6'),
+        new MinicondaEnv('3', '4.3.21', '10a4fa6'),
+      ]
 
-  // NOOP / DRY_RUN
-  stage('git tag') {
-    retry(retries) {
-      timeout(time: 1, unit: 'HOURS') {
-        // needs eups distrib tag to be sync'd from s3 -> k8s volume
-        build job: 'release/tag-git-repos',
-          parameters: [
-            string(name: 'BUILD_ID', value: bx),
-            string(name: 'GIT_TAG', value: gitTag),
-            booleanParam(name: 'DRY_RUN', value: true)
-          ]
-      }
-    }
-  }
+      def platform = [:]
 
-  stage("build eups tarballs") {
-    def operatingsystem = [
-      'centos-7',
-      'centos-6',
-      'osx-10.11',
-    ]
-
-    def pyenv = [
-      new MinicondaEnv('2', '4.2.12', '7c8e67'), // keep until v14_0
-      new MinicondaEnv('3', '4.2.12', '7c8e67'), // keep until v14_0
-      new MinicondaEnv('2', '4.3.21', '10a4fa6'),
-      new MinicondaEnv('3', '4.3.21', '10a4fa6'),
-    ]
-
-    def platform = [:]
-
-    operatingsystem.each { os ->
-      pyenv.each { py ->
-        platform["${os}.${py.slug()}"] = {
-          retry(retries) {
-            timeout(time: 6, unit: 'HOURS') {
-              build job: 'release/tarball',
-                parameters: [
-                  string(name: 'PRODUCT', value: 'lsst_distrib'),
-                  string(name: 'EUPS_TAG', value: eupsTag),
-                  booleanParam(name: 'SMOKE', value: true),
-                  booleanParam(name: 'RUN_DEMO', value: true),
-                  booleanParam(name: 'PUBLISH', value: true),
-                  string(name: 'PYVER', value: py.pythonVersion),
-                  string(name: 'MINIVER', value: py.minicondaVersion),
-                  string(name: 'LSSTSW_REF', value: py.lsstswRef),
-                  string(name: 'OS', value: os),
-                ]
+      operatingsystem.each { os ->
+        pyenv.each { py ->
+          platform["${os}.${py.slug()}"] = {
+            retry(retries) {
+              timeout(time: 6, unit: 'HOURS') {
+                build job: 'release/tarball',
+                  parameters: [
+                    string(name: 'PRODUCT', value: 'lsst_distrib'),
+                    string(name: 'EUPS_TAG', value: eupsTag),
+                    booleanParam(name: 'SMOKE', value: true),
+                    booleanParam(name: 'RUN_DEMO', value: true),
+                    booleanParam(name: 'PUBLISH', value: true),
+                    string(name: 'PYVER', value: py.pythonVersion),
+                    string(name: 'MINIVER', value: py.minicondaVersion),
+                    string(name: 'LSSTSW_REF', value: py.lsstswRef),
+                    string(name: 'OS', value: os),
+                  ]
+              }
             }
           }
         }
       }
+
+      parallel platform
     }
 
-    parallel platform
-  }
+    stage('wait for s3 sync') {
+      sleep time: 15, unit: 'MINUTES'
+    }
 
-  stage('wait for s3 sync') {
-    sleep time: 15, unit: 'MINUTES'
-  }
-
-  stage('build stack image') {
-    retry(retries) {
-      timeout(time: 1, unit: 'HOURS') {
-        build job: 'release/docker/build-stack',
-          parameters: [
-            string(name: 'TAG', value: eupsTag)
-          ]
+    stage('build stack image') {
+      retry(retries) {
+        timeout(time: 1, unit: 'HOURS') {
+          build job: 'release/docker/build-stack',
+            parameters: [
+              string(name: 'TAG', value: eupsTag)
+            ]
+        }
       }
     }
-  }
 
-  stage('build jupyterlabdemo image') {
-    retry(retries) {
-      timeout(time: 1, unit: 'HOURS') {
-        // based on lsstsqre/stack image
-        build job: 'sqre/infrastructure/build-jupyterlabdemo',
-          parameters: [
-            string(name: 'TAG', value: eupsTag),
-            booleanParam(name: 'NO_PUSH', value: false),
-          ]
+    stage('build jupyterlabdemo image') {
+      retry(retries) {
+        timeout(time: 1, unit: 'HOURS') {
+          // based on lsstsqre/stack image
+          build job: 'sqre/infrastructure/build-jupyterlabdemo',
+            parameters: [
+              string(name: 'TAG', value: eupsTag),
+              booleanParam(name: 'NO_PUSH', value: false),
+            ]
+        }
       }
     }
-  }
 
-  stage('archive') {
-    node {
-      results = [
-        bnnnn: bx,
-        eups_tag: eupsTag
-      ]
+  } finally {
+    stage('archive') {
+      results = []
+      if (bx) {
+        results['bnnn'] = bx
+      }
+      if (gitTag) {
+        results['git_tag'] = gitTag
+      }
+      if (euptsTag) {
+        results['eups_tag'] = eupsTag
+      }
+
       util.dumpJson('results.json', results)
 
       archiveArtifacts([
@@ -208,7 +219,7 @@ try {
         fingerprint: true
       ])
     }
-  }
+  } // try
 } catch (e) {
   // If there was an exception thrown, the build failed
   currentBuild.result = "FAILED"
