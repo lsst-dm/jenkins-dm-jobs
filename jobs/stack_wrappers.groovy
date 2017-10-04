2ce1b8b45fd2049ba6599084f0f1aba339216de0
Joshua Hoblitt <josh@hoblitt.com>
2017-10-04 15:25:19 -0700

    rm ci_hsc job
    ci_hsc is now py3 compatible and can be merged into
    science-pipelines/lsst_distrib.

--- jobs/stack_wrappers.groovy
+++ jobs/stack_wrappers.groovy
@@ -6,11 +6,6 @@ import util.StackOsMatrix
 def py2Job = 'science-pipelines/py2-boondocks'
 
 [
-  [
-    product: 'ci_hsc',
-    skip_demo: true,
-    triggerJob: py2Job,
-  ],
 ].each { j ->
   def stack = new StackOsMatrix(j)
 
