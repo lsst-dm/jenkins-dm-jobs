96ac43319ae80124d2c02cc77dd6663afe0214ff
Joshua Hoblitt <josh@hoblitt.com>
2017-10-04 09:23:37 -0700

    rm ci_ctio0m9 job

--- jobs/stack_wrappers.groovy
+++ jobs/stack_wrappers.groovy
@@ -12,11 +12,6 @@ def py2Job = 'science-pipelines/py2-boondocks'
     triggerJob: py2Job,
   ],
   [
-    product: 'ci_ctio0m9',
-    skip_demo: true,
-    triggerJob: py2Job,
-  ],
-  [
     name: 'sims/build',
     product: 'lsst_sims',
     branch: '13.0 v13.0',
