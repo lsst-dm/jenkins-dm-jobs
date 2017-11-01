d5072f6bcd728b17fb572773257143859fd54988
Joshua Hoblitt <josh@hoblitt.com>
2017-11-01 10:25:41 -0700

    add missing run() method

--- pipelines/release/tag_git_repos.groovy
+++ pipelines/release/tag_git_repos.groovy
@@ -17,42 +17,44 @@ node('jenkins-master') {
 notify.wrap {
   def timelimit = params.TIMEOUT.toInteger()
 
-  withCredentials([[
-    $class: 'StringBinding',
-    credentialsId: 'github-api-token-sqreadmin',
-    variable: 'GITHUB_TOKEN'
-  ]]) {
-    util.shColor '''
-      #!/bin/bash -e
+  def run = {
+    withCredentials([[
+      $class: 'StringBinding',
+      credentialsId: 'github-api-token-sqreadmin',
+      variable: 'GITHUB_TOKEN'
+    ]]) {
+      util.shColor '''
+        #!/bin/bash -e
 
-      ARGS=()
-      if [[ $DRY_RUN == "true" ]]; then
-        ARGS+=('--dry-run')
-      fi
+        ARGS=()
+        if [[ $DRY_RUN == "true" ]]; then
+          ARGS+=('--dry-run')
+        fi
 
-      # do not echo GH token to console log
-      set +x
-      ARGS+=('--token' "$GITHUB_TOKEN")
-      set -x
+        # do not echo GH token to console log
+        set +x
+        ARGS+=('--token' "$GITHUB_TOKEN")
+        set -x
 
-      ARGS+=('--org' 'lsst')
-      ARGS+=('--team' 'Data Management')
-      ARGS+=('--email' 'sqre-admin@lists.lsst.org')
-      ARGS+=('--tagger' 'sqreadmin')
-      ARGS+=('--fail-fast')
-      ARGS+=('--debug')
-      ARGS+=("$GIT_TAG")
-      ARGS+=("$BUILD_ID")
+        ARGS+=('--org' 'lsst')
+        ARGS+=('--team' 'Data Management')
+        ARGS+=('--email' 'sqre-admin@lists.lsst.org')
+        ARGS+=('--tagger' 'sqreadmin')
+        ARGS+=('--fail-fast')
+        ARGS+=('--debug')
+        ARGS+=("$GIT_TAG")
+        ARGS+=("$BUILD_ID")
 
-      virtualenv venv
-      . venv/bin/activate
-      pip install sqre-codekit==3.1.0
+        virtualenv venv
+        . venv/bin/activate
+        pip install sqre-codekit==3.1.0
 
-      # do not echo GH token to console log
-      set +x
-      github-tag-version "${ARGS[@]}"
-    '''
-  }
+        # do not echo GH token to console log
+        set +x
+        github-tag-version "${ARGS[@]}"
+      '''
+    } // withCredentials
+  } // run
 
   // python 2.7 is required
   node('centos-7') {
