ccc3c5bc4b59017decf72b089dc342b2f10c2854
Joshua Hoblitt <josh@hoblitt.com>
2017-11-07 13:05:55 -0700

    add validate_drp to build-stack default products

--- jobs/build_stack.groovy
+++ jobs/build_stack.groovy
@@ -7,7 +7,7 @@ pipelineJob("${folder}/build-stack") {
   description('Constructs docker images with EUPS tarballs.')
 
   parameters {
-    stringParam('PRODUCT', 'lsst_distrib', 'Whitespace delimited list of EUPS products to build.')
+    stringParam('PRODUCT', 'lsst_distrib validate_drp', 'Whitespace delimited list of EUPS products to build.')
     stringParam('TAG', null, 'EUPS distrib tag name. Eg. w_2016_08')
     stringParam('TIMEOUT', '1', 'build timeout in hours')
   }
