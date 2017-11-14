5ee70823bf68cfbda774d406af7ae8f423ed9f65
Joshua Hoblitt <josh@hoblitt.com>
2017-11-14 10:29:53 -0700

    add build-postqa job

---
+++ pipelines/sqre/infrastructure/build_postqa.groovy
@@ -0,0 +1,50 @@
+def notify = null
+
+node('jenkins-master') {
+  dir('jenkins-dm-jobs') {
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs(),
+      changelog: false,
+      poll: false
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+  }
+}
+
+notify.wrap {
+
+  def image      = null
+  def hubRepo    = 'lsstsqre/postqa'
+  def githubRepo = 'lsst-sqre/docker-postqa'
+  def githubRef  = 'master'
+  def postqaVer  = '1.3.2'
+
+  node('docker') {
+    stage('checkout') {
+      git([
+        url: "https://github.com/${githubRepo}",
+        branch: githubRef,
+      ])
+    }
+
+    stage('build') {
+      // ensure base image is always up to date
+      docker.image('docker.io/centos:7').pull()
+
+      image = docker.build("${hubRepo}", "--no-cache --build-arg POSTQA_VER=${postqaVer} .")
+    }
+
+    stage('push') {
+      if (! params.NO_PUSH) {
+        docker.withRegistry(
+          'https://index.docker.io/v1/',
+          'dockerhub-sqreadmin'
+        ) {
+          image.push(postqaVer)
+        }
+      }
+    } // push
+  } // node
+} // notify.wrap
