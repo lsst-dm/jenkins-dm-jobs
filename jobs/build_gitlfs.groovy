02023a7a274df4ad9fcda7f3981f244aa9321a76
Joshua Hoblitt <josh@hoblitt.com>
2017-11-16 17:17:21 -0700

    add LFS_VER and LATEST parameters
    * The boolean LATEST parameter will cause the constructed image to also
    be pushed to the docker registry with the 'latest' tag.

--- jobs/build_gitlfs.groovy
+++ jobs/build_gitlfs.groovy
@@ -7,7 +7,9 @@ pipelineJob("${folder}/build-gitlfs") {
   description('Constructs docker git-lfs images.')
 
   parameters {
+    stringParam('LFS_VER', '2.3.4', 'Git lfs version to install')
     booleanParam('NO_PUSH', false, 'Do not push image to docker registry.')
+    booleanParam('LATEST', false, 'Also push to docker registry with "latest" tag.')
   }
 
   properties {
