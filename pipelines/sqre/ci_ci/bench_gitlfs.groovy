e73ffad2700148995cfd9eae8d8216a1e761902b
Joshua Hoblitt <josh@hoblitt.com>
2017-11-16 18:53:11 -0700

    add time to benchmark docker image

--- pipelines/sqre/ci_ci/bench_gitlfs.groovy
+++ pipelines/sqre/ci_ci/bench_gitlfs.groovy
@@ -24,7 +24,7 @@ notify.wrap {
         def hub = "docker.io/lsstsqre/gitlfs:${lfsVer}"
         def local = "${hub}-local"
 
-        util.wrapContainer(hub, local)
+        wrapContainer(hub, local)
         def image = docker.image(local)
 
         runs.times {
@@ -59,3 +59,45 @@ notify.wrap {
     }
   } // node
 } // notify.wrap
+
+def void wrapContainer(String imageName, String tag) {
+  def buildDir = 'docker'
+  def config = util.dedent("""
+    FROM    ${imageName}
+
+    ARG     USER
+    ARG     UID
+    ARG     GROUP
+    ARG     GID
+    ARG     HOME
+
+    USER    root
+    RUN     groupadd -g \$GID \$GROUP
+    RUN     useradd -d \$HOME -g \$GROUP -u \$UID \$USER
+    RUN     yum install -y time
+
+    USER    \$USER
+    WORKDIR \$HOME
+  """)
+
+  // docker insists on recusrively checking file access under its execution
+  // path -- so run it from a dedicated dir
+  dir(buildDir) {
+    writeFile(file: 'Dockerfile', text: config)
+
+    util.shColor """
+      set -e
+      set -x
+
+      docker build -t "${tag}" \
+          --build-arg USER="\$(id -un)" \
+          --build-arg UID="\$(id -u)" \
+          --build-arg GROUP="\$(id -gn)" \
+          --build-arg GID="\$(id -g)" \
+          --build-arg HOME="\$HOME" \
+          .
+    """
+
+    deleteDir()
+  }
+}
