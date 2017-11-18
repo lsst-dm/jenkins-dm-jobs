d8b8709f1f15b7e2bbf1d908bb777cb3b758bfae
Joshua Hoblitt <josh@hoblitt.com>
2017-11-17 22:28:09 -0700

    git tags use '.'s not '-'s

--- jobs/clean_builds.groovy
+++ jobs/clean_builds.groovy
@@ -28,7 +28,7 @@ import util.CleanBuild
   [
     name: 'sims/lsst_sims',
     product: 'lsst_sims',
-    branch: 'w_2017_45',
+    branch: 'w.2017.45',
     skipDemo: true,
     skipDocs: true,
     seedJob: SEED_JOB,
