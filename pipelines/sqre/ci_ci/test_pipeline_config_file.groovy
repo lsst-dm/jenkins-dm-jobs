e0b35af87218229491dc27729899649af3b63fb2
Joshua Hoblitt <josh@hoblitt.com>
2017-12-05 10:05:43 -0700

    add test-pipeline-config-file job

---
+++ pipelines/sqre/ci_ci/test_pipeline_config_file.groovy
@@ -0,0 +1,23 @@
+def config = null
+
+node('jenkins-master') {
+  dir('jenkins-dm-jobs') {
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs(),
+      changelog: false,
+      poll: false
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+    util = load 'pipelines/lib/util.groovy'
+
+    config = readYaml(
+      text: readFile('pipelines/sqre/ci_ci/test_pipeline_config_file.yaml')
+    )
+  }
+}
+
+echo config['a']
+echo config['b']
+echo config['c']
