d96c532463cea923a01dc4bd279fba7ffdb4785f
danielsf <scott.f.daniel@gmail.com>
2018-01-19 15:58:43 -0800

    update lsst_sims to build against w.2018.01

--- jobs/clean_builds.groovy
+++ jobs/clean_builds.groovy
@@ -28,7 +28,7 @@ import util.CleanBuild
   [
     name: 'sims/lsst_sims',
     product: 'lsst_sims',
-    branch: 'w.2017.48',
+    branch: 'w.2018.01',
     skipDemo: true,
     skipDocs: true,
     seedJob: SEED_JOB,
