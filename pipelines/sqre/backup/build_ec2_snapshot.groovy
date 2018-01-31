3093f58f0fad733050ab52abfaee68873702734e
Joshua Hoblitt <josh@hoblitt.com>
2018-01-31 10:59:23 -0700

    add build-ec2-snapshot job

---
+++ pipelines/sqre/backup/build_ec2_snapshot.groovy
@@ -0,0 +1,68 @@
+node('jenkins-master') {
+  dir('jenkins-dm-jobs') {
+    checkout([
+      $class: 'GitSCM',
+      branches: scm.getBranches(),
+      userRemoteConfigs: scm.getUserRemoteConfigs(),
+      changelog: false,
+      poll: false
+    ])
+    notify = load 'pipelines/lib/notify.groovy'
+    util = load 'pipelines/lib/util.groovy'
+  }
+}
+
+notify.wrap {
+  def image      = null
+  def hubRepo    = 'lsstsqre/ec2-snapshot'
+  def githubSlug = 'lsst-sqre/ec2-snapshot'
+  def githubRepo = "https://github.com/${githubSlug}"
+  def githubRef  = 'master'
+  def dockerTag  = githubRef.tr('/', '_')
+  def dockerDir  = '.'
+  def pushLatest = params.LATEST
+  def noPush     = params.NO_PUSH
+
+  def run = {
+    def abbrHash = null
+
+    stage('checkout') {
+      git([
+        url: githubRepo,
+        branch: githubRef,
+      ])
+
+      abbrHash = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
+    }
+
+    stage('build') {
+      dir(dockerDir) {
+        // ensure base image is always up to date
+        image = docker.build("${hubRepo}", "--pull=true --no-cache . ")
+      }
+    }
+
+    stage('push') {
+      if (!noPush) {
+        docker.withRegistry(
+          'https://index.docker.io/v1/',
+          'dockerhub-sqreadmin'
+        ) {
+          image.push(dockerTag)
+          if (githubRef == 'master') {
+            image.push("g${abbrHash}")
+          }
+          if (pushLatest) {
+            image.push('latest')
+          }
+        }
+      }
+    } // push
+  } // run
+
+  node('docker') {
+    timeout(time: 30, unit: 'MINUTES') {
+      run()
+    }
+  } // node
+} // notify.wrap
