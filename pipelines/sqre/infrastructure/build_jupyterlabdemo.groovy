7825876a3df6b057b12bfe64b56df74dca33bb97
Joshua Hoblitt <josh@hoblitt.com>
2018-02-16 09:56:30 -0700

    rm custom @adam notifications
    Standard slack channel notifications settings are now able to replace
    hardcoded functionality.

--- pipelines/sqre/infrastructure/build_jupyterlabdemo.groovy
+++ pipelines/sqre/infrastructure/build_jupyterlabdemo.groovy
@@ -12,9 +12,7 @@ node('jenkins-master') {
   }
 }
 
-try {
-  notify.started()
-
+notify.wrap {
   def timelimit = params.TIMEOUT.toInteger()
 
   def run = {
@@ -61,40 +59,5 @@ try {
     timeout(time: timelimit, unit: 'HOURS') {
       run()
     }
-  }
-} catch (e) {
-  // If there was an exception thrown, the build failed
-  currentBuild.result = "FAILED"
-  throw e
-} finally {
-  echo "result: ${currentBuild.result}"
-  switch(currentBuild.result) {
-    case null:
-    case 'SUCCESS':
-      notify.success()
-      break
-    case 'ABORTED':
-      notify.aborted()
-      pingAdamAborted()
-      break
-    case 'FAILURE':
-      notify.failure()
-      pingAdamFailure()
-      break
-    default:
-      notify.failure()
-      pingAdamFailure()
-  }
-}
-
-def void pingAdam(String msg) {
-  slackSend color: 'danger', message: "<@U2VGYJN92> ${msg}"
-}
-
-def void pingAdamAborted() {
-  pingAdam(notify.slackAbortedMessage())
-}
-
-def void pingAdamFailure() {
-  pingAdam(notify.slackFailureMessage())
-}
+  } // node
+} // notify.wrap
