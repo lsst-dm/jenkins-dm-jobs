b2662c06bd59d5d094423f644a87bac26a64763c
Joshua Hoblitt <josh@hoblitt.com>
2018-03-22 13:15:46 -0700

    bump awscli version to 1.14.61
    To pickup working aws ec2 create-snapshot --tag-specifications.

--- jobs/build_awscli.groovy
+++ jobs/build_awscli.groovy
@@ -5,7 +5,7 @@ p.pipeline().with {
   description('Constructs docker awscli images.')
 
   parameters {
-    stringParam('AWSCLI_VER', '1.14.2', 'awscli version to install')
+    stringParam('AWSCLI_VER', '1.14.61', 'awscli version to install')
     booleanParam('NO_PUSH', false, 'Do not push image to docker registry.')
     booleanParam('LATEST', false, 'Also push to docker registry with "latest" tag.')
   }
