6549e9bf32a4d7660f84e9d0b2888ef47d5b9e48
Joshua Hoblitt <josh@hoblitt.com>
2018-04-05 17:06:14 -0700

    add build-documenteer-base job

---
+++ jobs/build_documenteer_base.groovy
@@ -0,0 +1,18 @@
+import util.Plumber
+
+def p = new Plumber(
+  name: 'sqre/infrastructure/build-documenteer-base',
+  dsl: this
+)
+p.pipeline().with {
+  description('Constructs docker images.')
+
+  parameters {
+    booleanParam('NO_PUSH', false, 'Do not push image to docker registry.')
+    booleanParam('LATEST', true, 'Also push to docker registry with "latest" tag.')
+  }
+
+  triggers {
+    upstream('sqre/infrastructure/build-newinstall', 'SUCCESS')
+  }
+}
