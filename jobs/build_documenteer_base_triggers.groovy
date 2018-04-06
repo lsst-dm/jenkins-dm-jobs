28303df7281728aad959e79f13c0034c7bed96b8
Joshua Hoblitt <josh@hoblitt.com>
2018-04-05 17:06:34 -0700

    add build-documenteer-base-triggers job
    This job's only function is to trigger build-documenteer-base from scm
    polling.

---
+++ jobs/build_documenteer_base_triggers.groovy
@@ -0,0 +1,28 @@
+import util.Common
+Common.makeFolders(this)
+
+def j = job('sqre/infrastructure/build-newinstall-triggers') {
+  def repo = SEED_JOB.scm.userRemoteConfigs.get(0).getUrl()
+  def ref  = SEED_JOB.scm.getBranches().get(0).getName()
+
+  scm {
+    git {
+      remote {
+        github('lsst-sqre/docker-documenteer-base')
+        branch('master')
+      }
+    }
+  } // scm
+
+  triggers {
+    githubPush()
+  }
+
+  steps {
+    downstreamParameterized {
+      trigger('sqre/infrastructure/build-documenteer-base') {}
+    }
+  }
+}
+
+Common.addNotification(j)
