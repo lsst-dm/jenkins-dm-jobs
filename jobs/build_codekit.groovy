3d1838f3529e9d1f697731718ffb41358c196c39
Joshua Hoblitt <josh@hoblitt.com>
2018-04-18 11:23:26 -0700

    bump default codekit version to 4.0.0

--- jobs/build_codekit.groovy
+++ jobs/build_codekit.groovy
@@ -5,7 +5,7 @@ p.pipeline().with {
   description('Constructs docker sqre-codekit images.')
 
   parameters {
-    stringParam('CODEKIT_VER', '3.1.0', 'sqre-codekit pypi version to install')
+    stringParam('CODEKIT_VER', '4.0.0', 'sqre-codekit pypi version to install')
     booleanParam('NO_PUSH', false, 'Do not push image to docker registry.')
     booleanParam('LATEST', false, 'Also push to docker registry with "latest" tag.')
   }
