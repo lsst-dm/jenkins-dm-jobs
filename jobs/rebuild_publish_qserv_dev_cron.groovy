a310e60b0fb37a4e81fa73efb9bc2fe0ada4c996
Joshua Hoblitt <josh@hoblitt.com>
2018-04-20 12:38:45 -0700

    add dax/release/rebuild_publish_qserv-dev-cron job

---
+++ jobs/rebuild_publish_qserv_dev_cron.groovy
@@ -0,0 +1,16 @@
+import util.Common
+Common.makeFolders(this)
+
+def j = job('dax/release/rebuild_publish_qserv-dev-cron', dsl: this)
+  triggers {
+    cron('0 0 * * 6')
+  }
+
+  steps {
+    downstreamParameterized {
+      trigger('dax/release/rebuild_publish_qserv-dev') {}
+    }
+  }
+}
+
+Common.addNotification(j)
