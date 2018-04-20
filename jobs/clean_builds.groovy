a41a1aca739ac7725c064016950c90dd15ee0782
Joshua Hoblitt <josh@hoblitt.com>
2018-04-20 12:45:56 -0700

    (TESTING) rm jobs not under test

--- jobs/clean_builds.groovy
+++ jobs/clean_builds.groovy
@@ -5,13 +5,6 @@ import util.CleanBuild
 
 [
   [
-    name: 'science-pipelines/lsst_distrib',
-    product: 'lsst_distrib ci_hsc',
-    skipDemo: false,
-    skipDocs: false,
-    seedJob: SEED_JOB,
-  ],
-  [
     name: 'dax/dax_webserv',
     product: 'dax_webserv',
     skipDemo: true,
@@ -25,14 +18,6 @@ import util.CleanBuild
     skipDemo: true,
     skipDocs: true,
     buildConfig: 'dax-lsstsw-matrix',
-    seedJob: SEED_JOB,
-  ],
-  [
-    name: 'sims/lsst_sims',
-    product: 'lsst_sims',
-    branch: 'w.2018.01',
-    skipDemo: true,
-    skipDocs: true,
     seedJob: SEED_JOB,
   ],
 ].each { j ->
