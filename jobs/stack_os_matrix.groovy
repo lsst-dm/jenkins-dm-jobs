1361bd36507631069f45d4e16290bbfa9405fc06
Gabriele Comoretto <gcomoretto@lsst.org>
2018-05-29 13:06:46 -0700

    Added dummy paarameter

--- jobs/stack_os_matrix.groovy
+++ jobs/stack_os_matrix.groovy
@@ -8,6 +8,7 @@ p.pipeline().with {
     stringParam('BRANCH', null, 'Whitespace delimited list of "refs" to attempt to build.  Priority is highest -> lowest from left to right.  "master" is implicitly appended to the right side of the list, if not specified.')
     stringParam('PRODUCT', 'lsst_distrib', 'Whitespace delimited list of EUPS products to build.')
     booleanParam('SKIP_DEMO', false, 'Do not run the demo after all packages have completed building.')
+    stringParam('DUMMY', 'dummy value','This parameter has been added for (dummy) test')
     // XXX testing only
     //booleanParam('NO_FETCH', false, 'Do not pull from git remote if branch is already the current ref. (This should generally be false outside of testing the CI system)')
   }
