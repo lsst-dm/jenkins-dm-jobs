0fb9cbc6cd24b7d60222a5173a61ba51c595399a
Joshua Hoblitt <josh@hoblitt.com>
2018-06-07 09:26:07 -0700

    add description blurb to github-tag-teams job

--- jobs/github_tag_teams.groovy
+++ jobs/github_tag_teams.groovy
@@ -2,6 +2,26 @@ import util.Plumber
 
 def p = new Plumber(name: 'release/codekit/github-tag-teams', dsl: this)
 p.pipeline().with {
+  def text = '''
+    Tag the head of the default branch of all repositories in a GitHub org
+    which belong to the specified team(s).
+
+    Example:
+
+      $ github-tag-release \
+        --debug \
+        --dry-run \
+        --delete \
+        --org 'lsst' \
+        --allow-team 'DM Auxilliaries' \
+        --deny-team 'DM Externals' \
+        --token "$GITHUB_TOKEN" \
+        --user 'sqreadmin' \
+        --email 'sqre-admin@lists.lsst.org' \
+        --tag 'w.2018.18'
+  '''
+  description(text.replaceFirst("\n","").stripIndent())
+
   parameters {
     stringParam('GIT_TAG', null, 'git tag string. Eg. w.2016.08')
     booleanParam('DRY_RUN', true, 'Dry run')
