e4b16b77832bf71905a2d6d3b958b47115d54648
Joshua Hoblitt <josh@hoblitt.com>
2018-06-22 10:35:48 -0700

    add sqre/infra/build-s3sync job

---
+++ jobs/build_s3sync.groovy
@@ -0,0 +1,16 @@
+import util.Plumber
+import org.yaml.snakeyaml.Yaml
+
+def sqre = new Yaml().load(readFileFromWorkspace('etc/sqre/config.yaml'))
+
+def p = new Plumber(name: 'sqre/infrastructure/build-s3sync', dsl: this)
+p.pipeline().with {
+  description('Constructs s3sync docker images.')
+
+  parameters {
+    stringParam('AWSCLI_VER', sqre.awscli.pypi.version, 'awscli version to install')
+
+    booleanParam('PUBLISH', true, 'Push image to docker registry.')
+    booleanParam('LATEST', false, 'Also push to docker registry with `latest` tag.')
+  }
+}
