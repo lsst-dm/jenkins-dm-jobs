ee6de303bc7d2497f51e3323109a0d8aa5f66fd3
Joshua Hoblitt <josh@hoblitt.com>
2018-06-25 10:04:43 -0700

    add MANIFEST_ONLY param

--- pipelines/release/codekit/github_tag_release.groovy
+++ pipelines/release/codekit/github_tag_release.groovy
@@ -15,27 +15,34 @@ node('jenkins-master') {
 
 notify.wrap {
   util.requireParams([
-    'MANIFEST_ID',
     'DRY_RUN',
     'EUPS_TAG',
     'GIT_TAG',
     'LIMIT',
+    'MANIFEST_ID',
+    'MANIFEST_ONLY',
     'VERIFY',
   ])
 
-  String manifestId = params.MANIFEST_ID
-  Boolean dryRun    = params.DRY_RUN
-  String eupsTag    = params.EUPS_TAG
-  String gitTag     = params.GIT_TAG
-  String limit      = params.LIMIT // using as string; do not convert to int
-  Boolean verify    = params.VERIFY
+  // note that EUPS_TAG and MANIFEST_ONLY are mutually exclusive -- because of
+  // this, EUPS_TAG is "optional"
+  Boolean dryRun      = params.DRY_RUN
+  String eupsTag      = util.emptyToNull(params.EUPS_TAG) // '' means null
+  String gitTag       = params.GIT_TAG
+  String limit        = params.LIMIT // using as string; do not convert to int
+  String manifestId   = params.MANIFEST_ID
+  String manifestOnly = params.MANIFEST_ONLY
+  Boolean verify      = params.VERIFY
 
   options = [
     '--org': config.release_tag_org,
     '--dry-run': dryRun,
     '--manifest': manifestId,
-    '--eups-tag': eupsTag,
   ]
+
+  if (eupsTag) {
+    options.'--eups-tag' = eupsTag
+  }
 
   if (limit) {
     options.'--limit' = limit
@@ -43,6 +50,10 @@ notify.wrap {
 
   if (verify) {
     options.'--verify' = true
+  }
+
+  if (manifestOnly) {
+    options.'--manifest-only' = true
   }
 
   node('docker') {
