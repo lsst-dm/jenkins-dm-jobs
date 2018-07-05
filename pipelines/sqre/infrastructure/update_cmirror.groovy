de33a930ffd85829e307535436f434bf4604f867
Joshua Hoblitt <josh@hoblitt.com>
2018-07-05 11:23:24 -0700

    use abs path to conda-mirror prog

--- pipelines/sqre/infrastructure/update_cmirror.groovy
+++ pipelines/sqre/infrastructure/update_cmirror.groovy
@@ -130,7 +130,7 @@ def void mirrorCondaChannel(Map p) {
         pull: true,
       ) {
         util.bash '''
-          conda-mirror \
+          /miniconda/bin/conda-mirror
             --num-threads 0 \
             --upstream-channel "$UPSTREAM_URL" \
             --temp-directory "$TMP_DIR" \
