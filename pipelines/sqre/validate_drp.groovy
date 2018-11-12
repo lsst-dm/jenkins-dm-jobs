38ce469ffbed35b6b4e41fd12ecb5d71819d2be8
Joshua Hoblitt <josh@hoblitt.com>
2018-11-12 13:54:15 -0700

    migrate validate_drp sh script fragment to ci-scripts
    as `run_validate_drp.sh`.

--- pipelines/sqre/validate_drp.groovy
+++ pipelines/sqre/validate_drp.groovy
@@ -221,10 +221,10 @@ def void verifyDataset(Map p) {
     ) {
       if (buildCode) {
         buildDrp(
-          homeDir: homeDir,
           codeDir: codeDir,
-          runSlug: p.slug,
           ciDir: ciDir,
+          homeDir: homeDir,
+          runSlug: p.slug,
           lsstCompiler: lsstCompiler,
         )
       }
@@ -232,6 +232,7 @@ def void verifyDataset(Map p) {
       runDrp(
         runDir: runDir,
         codeDir: codeDir,
+        ciDir: ciDir,
         datasetName: ds.name,
         datasetDir: datasetDir,
       )
@@ -349,6 +350,7 @@ def void buildDrp(Map p) {
  * @param p.runDir String runtime cwd for validate_drp
  * @param p.datasetName String full name of the validation dataset
  * @param p.datasetDir String path to validation dataset
+ * @param p.ciDir String
  * @param p.codeDir (Optional) String path to validate_drp (code)
  */
 def void runDrp(Map p) {
@@ -356,144 +358,27 @@ def void runDrp(Map p) {
     'runDir',
     'datasetName',
     'datasetDir',
+    'ciDir',
   ])
 
   p = [codeDir: ''] + p
 
-  // run drp driver script
-  def run = {
-    util.bash '''
-      find_mem() {
-        # Find system available memory in GiB
-        local os
-        os=$(uname)
-
-        local sys_mem=""
-        case $os in
-          Linux)
-            [[ $(grep MemAvailable /proc/meminfo) =~ \
-               MemAvailable:[[:space:]]*([[:digit:]]+)[[:space:]]*kB ]]
-            sys_mem=$((BASH_REMATCH[1] / 1024**2))
-            ;;
-          Darwin)
-            # I don't trust this fancy greppin' an' matchin' in the shell.
-            local free=$(vm_stat | grep 'Pages free:'     | \
-              tr -c -d [[:digit:]])
-            local inac=$(vm_stat | grep 'Pages inactive:' | \
-              tr -c -d [[:digit:]])
-            sys_mem=$(( (free + inac) / ( 1024 * 256 ) ))
-            ;;
-          *)
-            >&2 echo "Unknown uname: $os"
-            exit 1
-            ;;
-        esac
-
-        echo "$sys_mem"
-      }
-
-      # find the maximum number of processes that may be run on the system
-      # given the the memory per core ratio in GiB -- may be expressed in
-      # floating point.
-      target_cores() {
-        local mem_per_core=${1:-1}
-
-        local sys_mem=$(find_mem)
-        local sys_cores
-        sys_cores=$(getconf _NPROCESSORS_ONLN)
-
-        # bash doesn't support floating point arithmetic
-        local target_cores
-        #target_cores=$(echo "$sys_mem / $mem_per_core" | bc)
-        target_cores=$(awk "BEGIN{ print int($sys_mem / $mem_per_core) }")
-        [[ $target_cores > $sys_cores ]] && target_cores=$sys_cores
-
-        echo "$target_cores"
-      }
-
-      set +o xtrace
-      source /opt/lsst/software/stack/loadLSST.bash
-
-      # if CODE_DIR is defined, set that up instead of the default validate_drp
-      # product
-      if [[ -n $CODE_DIR ]]; then
-        setup -k -r "$CODE_DIR"
-      else
-        setup validate_drp
-      fi
-
-      setup -k -r "$DATASET_DIR"
-      set -o xtrace
-
-      case "$DATASET" in
-        validation_data_cfht)
-          RUN="$VALIDATE_DRP_DIR/examples/runCfhtTest.sh"
-          RESULTS=(
-            Cfht_output_r.json
-          )
-          LOGS=(
-            'Cfht/singleFrame.log'
-            'job_validate_drp.log'
-          )
-          ;;
-        validation_data_decam)
-          RUN="$VALIDATE_DRP_DIR/examples/runDecamTest.sh"
-          RESULTS=(
-            Decam_output_z.json
-          )
-          LOGS=(
-            'Decam/singleFrame.log'
-            'job_validate_drp.log'
-          )
-          ;;
-        validation_data_hsc)
-          RUN="$VALIDATE_DRP_DIR/examples/runHscTest.sh"
-          RESULTS=(
-            Hsc_output_HSC-I.json
-            Hsc_output_HSC-R.json
-            Hsc_output_HSC-Y.json
-          )
-          LOGS=(
-            'Hsc/singleFrame.log'
-            'job_validate_drp.log'
-          )
-          ;;
-        *)
-          >&2 echo "Unknown DATASET: ${DATASET}"
-          exit 1
-          ;;
-      esac
-
-      # pipe_drivers mpi implementation uses one core for orchestration, so we
-      # need to set NUMPROC to the number of cores to utilize + 1
-      MEM_PER_CORE=2.0
-      export NUMPROC=$(($(target_cores $MEM_PER_CORE) + 1))
-
-      set +e
-      "$RUN" -- --noplot
-      run_status=$?
-      set -e
-
-      echo "${RUN##*/} - exit status: ${run_status}"
-
-      # bail out if the drp output file is missing
-      if [[ ! -e  ${RESULTS[0]} ]]; then
-        echo "drp result file does not exist: ${RESULTS[0]}"
-        exit 1
-      fi
-
-      exit $run_status
-    '''
-  } // run
-
   withEnv([
-    "CODE_DIR=${p.codeDir}",
-    "DATASET=${p.datasetName}",
-    "DATASET_DIR=${p.datasetDir}",
+    "LSST_VALIDATE_DRP_CODE_DIR=${p.codeDir}",
+    "LSST_VALIDATE_DRP_DATASET=${p.datasetName}",
+    "LSST_VALIDATE_DRP_DATASET_DIR=${p.datasetDir}",
+    "LSST_CI_SCRIPTS_DIR=${p.ciDir}",
   ]) {
     try {
       dir(p.runDir) {
-        run()
+        // run drp driver script
+        util.bash '''
+          set +o xtrace
+          source /opt/lsst/software/stack/loadLSST.bash
+          set -o xtrace
+
+          "${LSST_CI_SCRIPTS_DIR}/run_validate_drp.sh"
+        '''
       }
     } finally {
       // archive from root of ws
