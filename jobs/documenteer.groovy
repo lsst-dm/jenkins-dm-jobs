af2c62c1993551df70c3624ba41bb9fbd86ad6f1
Joshua Hoblitt <josh@hoblitt.com>
2018-12-07 16:27:55 -0700

    make git/eups tag job param examples consistent with sqr-016

--- jobs/documenteer.groovy
+++ jobs/documenteer.groovy
@@ -5,7 +5,7 @@ p.pipeline().with {
   description('Build and publish documenteer/sphinx based docs.')
 
   parameters {
-    stringParam('EUPS_TAG', null, '(required) EUPS distrib tag name. Eg. w_2017_45')
+    stringParam('EUPS_TAG', null, '(required) EUPS distrib tag name. Eg. w_9999_52')
     stringParam('LTD_SLUG', null, '(required) LTD edition slug')
     stringParam('TEMPLATE_REPO', 'lsst/pipelines_lsst_io', 'github repo slug')
     stringParam('TEMPLATE_REF', 'master', 'git repo ref')
--- jobs/official_release.groovy
+++ jobs/official_release.groovy
@@ -9,15 +9,15 @@ p.pipeline().with {
     A new git repo tag will be applied AND a new eups tag will be published.
     The new eups tag is derived from the git tag name.
 
-    Eg., (git tag) `16.0.0.rc1` -> (eups tag) `16_0_0_rc1`
+    Eg., (git tag) `888.0.0.rc1` -> (eups tag) `888_0_0_rc1`
   '''
   description(text.replaceFirst("\n","").stripIndent())
 
   parameters {
     stringParam('SOURCE_GIT_REFS', null,
-      'existing git ref(s) upon which to base the release. Eg. "w.2018.22"')
+      'existing git ref(s) upon which to base the release. Eg. "w.9999.52"')
     stringParam('RELEASE_GIT_TAG', null,
-      'git tag for the new release. Eg. "v16.0.rc1"')
+      'git tag for the new release. Eg. "v888.0.0.rc1"')
     booleanParam('O_LATEST', false,
       'update the eups "O_LATEST" tag -- should only be done for a final (non-rc) release')
   }
