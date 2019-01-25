d6d046a8471c1c51d4493ac7e5ac27521901e664
adam <athornton@gmail.com>
2019-01-25 09:43:59 -0700

    Replace 'jld'/'jupyterlabdemo' nomenclature.

--- jobs/build_sciplatlab.groovy
+++ jobs/build_sciplatlab.groovy
@@ -1,18 +1,18 @@
 import util.Plumber
 
 def p = new Plumber(
-  name: 'sqre/infra/build-jupyterlabdemo',
+  name: 'sqre/infra/build-sciplatlab,
   dsl: this
 )
 p.pipeline().with {
-  description('Constructs docker jupyterlabdemo images.')
+  description('Constructs docker LSST Science Platform Notebook Aspect images.')
 
   parameters {
     stringParam('TAG', null, 'eups distrib tag')
     booleanParam('NO_PUSH', false, 'Do not push image to docker registry.')
     stringParam('PYVER', '3', 'Python version')
     stringParam('BASE_IMAGE', 'lsstsqre/centos', 'Base Docker image')
-    stringParam('IMAGE_NAME', 'lsstsqre/jld-lab', 'Output image name')
+    stringParam('IMAGE_NAME', 'lsstsqre/sciplat-lab', 'Output image name')
     stringParam('TAG_PREFIX', '7-stack-lsst_distrib-', 'Tag prefix')
     stringParam('TIMEOUT', '1', 'build timeout in hours')
   }
