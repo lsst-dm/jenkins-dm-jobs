7d420169a4679416632dae14c4b4ad1dc76837b3
Krzysztof Findeisen <kfindeis@uw.edu>
2020-08-19 17:06:19 -0500

    Fix syntax error in ap_verify.groovy.

--- pipelines/scipipe/ap_verify.groovy
+++ pipelines/scipipe/ap_verify.groovy
@@ -428,8 +428,8 @@ def void runApVerify(Map p) {
       dir(p.archiveDir) {
         util.record(util.xz([
           "${p.runDir}/**/*.log",
-          "${p.runDir}/**/*.json",  # Gen 2
-          "${p.runDir}/**/metricvalue_*/**/*.yaml",  # Gen 3
+          "${p.runDir}/**/*.json",  // Gen 2
+          "${p.runDir}/**/metricvalue_*/**/*.yaml",  // Gen 3
           "${p.runDir}/**/*.db",
         ]))
       }
