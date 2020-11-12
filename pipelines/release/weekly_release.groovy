c0804946f73427a53e22dd469d52eef571e5601d
adam <athornton@gmail.com>
2020-11-12 09:58:23 -0700

    reflect post-summit-redeploy image coupling

--- pipelines/release/weekly_release.groovy
+++ pipelines/release/weekly_release.groovy
@@ -176,39 +176,26 @@ notify.wrap {
       //  stages within other parallel stages, so we do these all
       //  sequentially in order not to have them held up by validate_drp.
 
-      stage('SAL-sciplat-lab linked image set') {
+      stage('SAL-sciplat-lab linked image set for teststands') {
         retry(retries) {
           build(
             job: 'sqre/infra/aggregate-sal',
             parameters: [
               string(name: 'TAG', value: eupsTag),
-              string(name: 'ENVIRONMENTS', value: 'nts tts'),
+              string(name: 'ENVIRONMENTS', value: 'nts tts kueyen'),
             ],
             wait: false,
           )
         } // retry
       } // stage
 
-      stage('SAL-sciplat-lab "base" and "kueyen" image') {
+      stage('SAL-sciplat-lab "base" and "summit" image') {
         retry(retries) {
           build(
             job: 'sqre/infra/aggregate-sal',
             parameters: [
               string(name: 'TAG', value: eupsTag),
-              string(name: 'ENVIRONMENTS', value: 'base kueyen'),
-            ],
-            wait: false,
-          )
-        } //retry
-      } // stage
-
-      stage('SAL-sciplat-lab "summit" image') {
-        retry(retries) {
-          build(
-            job: 'sqre/infra/aggregate-sal',
-            parameters: [
-              string(name: 'TAG', value: eupsTag),
-              string(name: 'ENVIRONMENTS', value: 'summit'),
+              string(name: 'ENVIRONMENTS', value: 'base summit'),
             ],
             wait: false,
           )
