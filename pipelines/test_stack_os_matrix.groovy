90f57f0503ef98e123ab123eadb6841d829c5d55
Kian-Tat Lim <ktl@slac.stanford.edu>
2022-08-14 10:16:51 -0700

    Allow usdf labels.

--- pipelines/test_stack_os_matrix.groovy
+++ pipelines/test_stack_os_matrix.groovy
@@ -8,7 +8,7 @@ node('jenkins-manager') {
       poll: false
     ])
     notify = load 'pipelines/lib/notify.groovy'
-    util = load 'pipelines/lib/util.groovy'
+    util = load 'pipelines/lib/util_test.groovy'
     scipipe = util.scipipeConfig() // needed for side effects
     sqre = util.sqreConfig() // needed for side effects
   }
