2efd68edb140cc232fa7e9e719ecc8c568df650c
James Tocknell <james.tocknell@mq.edu.au>
2022-08-24 16:49:47 +1000

    Add id to github branch source for new job-dsl

--- jobs/ci_scripts.groovy
+++ jobs/ci_scripts.groovy
@@ -4,6 +4,7 @@ Common.makeFolders(this)
 multibranchPipelineJob('sqre/infra/ci-scripts') {
   branchSources {
     github {
+      id('sqre/infra/ci-scripts')
       repoOwner('lsst-sqre')
       repository('ci-scripts')
       scanCredentialsId('github-user-sqreadmin')
