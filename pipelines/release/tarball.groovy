80831828183e1e5a81e4a1018c211cf81ed65a54
aranabhat <138172063+aranabhat@users.noreply.github.com>
2024-02-01 11:13:59 +1100

    removed /stack from eups site reference in tarball.groovy

--- pipelines/release/tarball.groovy
+++ pipelines/release/tarball.groovy
@@ -846,7 +846,7 @@ def String scriptPreamble(
     set -o pipefail
 
     if [[ -n \$EUPS_S3_BUCKET ]]; then
-        export LSST_EUPS_PKGROOT_BASE_URL="https://\${EUPS_BASE_URL}/stack"
+        export LSST_EUPS_PKGROOT_BASE_URL="https://\${EUPS_BASE_URL}"
     fi
 
     # isolate eups cache files
