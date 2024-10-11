4508c0a63e809d878cb81e8ed337d434f842de45
Kian-Tat Lim <ktl@slac.stanford.edu>
2024-10-11 00:53:02 +0000

    Update list of available node labels.

--- jobs/tarball.groovy
+++ jobs/tarball.groovy
@@ -20,7 +20,7 @@ p.pipeline().with {
     booleanParam('WIPEOUT', false, 'Completely wipe out workspace(s) before starting build.')
     stringParam('TIMEOUT', '23', 'build timeout in hours')
     stringParam('IMAGE', null, 'Pipeline base docker image (e.g. docker.io/lsstdm/scipipe-base:8 or empty for macOS)')
-    choiceParam('LABEL', ['centos-8-conda', 'centos-7-conda', 'osx-13', 'osx-14'], 'Jenkins build agent label')
+    choiceParam('LABEL', ['docker', 'arm64', 'osx-13', 'osx-14'], 'Jenkins build agent label')
     stringParam('COMPILER', 'conda-system', 'compiler version string')
     choiceParam('PYTHON_VERSION', ['3'], 'Python major version')
     stringParam('MINIVER', scipipe.template.tarball_defaults.miniver, 'Miniconda installer version')
